a0ad6c2f6e1504d0f8775833407c2a5c51ca704a
paulagarcia <paula.garcia.pereira@gmail.com>
2019-02-25 20:01:08 +0100

    reduce noticias a 6 e imagen asamblea

--- server/routes/api/noticias.js
+++ server/routes/api/noticias.js
@@ -2,7 +2,7 @@ const Noticias = require('../../models/Noticias');
 
 module.exports = (app) => {
   app.get('/api/noticias', (req, res, next) => {
-    Noticias.find({}).limit(9).sort({ fecha: -1 }).then(function (notas) {
+    Noticias.find({}).limit(6).sort({ fecha: -1 }).then(function (notas) {
       res.send(notas);
     });
   });
